024e7d54cc7c28822f4fbc2d8ef874ef2eb5dd34
abayasaye <abay400@gmail.com>
2022-09-27 23:19:52 +0300

    a

--- src/components/pages/HomeComponent/CatComponent.test.js
+++ src/components/pages/HomeComponent/CatComponent.test.js
@@ -4,7 +4,7 @@ import CatComp from './CatComponent'
 
 describe("tests for CatComponent ",()=>{
   it("return hello cats ",()=>{
-    const div = document.createElement("div")
+    const div  =  document.createElement("div")
     ReactDOM.render(<CatComp/>, div)
     expect(div.innerHTML).toEqual(`<div class="cat-component"><h1>hello cats</h1></div>`)
   })
